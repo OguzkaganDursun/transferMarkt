6737c5c81bd13b753b2b99ab7ded7cbf10920531
Oğuzkağan Dursun <dursunoguzkagan@gmail.com>
2024-01-14 15:03:42 +0300

    Update
    Update

--- script.js
+++ script.js
@@ -6,12 +6,11 @@ function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-// Show Slides Function
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
-// Slaytları gösterme fonksiyonu
+// Show Slides Function
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
